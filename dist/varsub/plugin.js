ee78790f27c1a0b70ad0dd72470ac8a4b371f3c5
Abhisek Pattnaik <abhisekp@engineer.com>
2022-11-03 15:06:29 +0530

    Add dist/ dir

---
+++ dist/varsub/plugin.js
@@ -0,0 +1,1 @@
+!function(){"use strict";tinymce.PluginManager.add("varsub",function(t,n){t.ui.registry.addButton("varsub",{text:"varsub button",onAction:function(){t.setContent("<p>content added from varsub</p>")}})})}();
